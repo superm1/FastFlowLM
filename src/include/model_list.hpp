8064355df5bb98a935ba19aea5ab22d6240ab348
ZaneNi <zhihenn@clemson.edu>
2026-02-17 14:33:43 -0500

    feat: support translategemma

--- src/include/model_list.hpp
+++ src/include/model_list.hpp
@@ -15,7 +15,7 @@
 #include <filesystem>
 #include "utils/utils.hpp"
 
-#define __FLM_VERSION__ "0.9.33"
+#define __FLM_VERSION__ "0.9.34"
 
 /// \note This class is used to manage the model list.
 class model_list {
